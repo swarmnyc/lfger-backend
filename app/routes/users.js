fc2600903a8c2ce45f44d529d94b530db58aa1fb
Joey Lappin <joey@swarmnyc.com>
2016-01-17 16:03:05 -0500

    Removed extraneous routes on User router

--- app/routes/users.js
+++ app/routes/users.js
@@ -45,41 +45,41 @@ module.exports = function(app) {
     });
   });
 
-  router.get('/:user', app.middleware.ensureLogin, function(req, res) {
-    res.status(200).json(req.models.user);
-  });
+  // router.get('/:user', app.middleware.ensureLogin, function(req, res) {
+  //   res.status(200).json(req.models.user);
+  // });
 
-  router.put('/:user', app.middleware.ensureLogin, function(req, res) {
-    let data = req.body;
-    let user = req.models.user;
+  // router.put('/:user', app.middleware.ensureLogin, function(req, res) {
+  //   let data = req.body;
+  //   let user = req.models.user;
+  //
+  //   if (!data) {
+  //     return res.status(403).json({ success: false, error: 'No data' });
+  //   }
+  //
+  //   async.each(Object.keys(data), function(key, callback) {
+  //     user[key] = data[key];
+  //     callback();
+  //   }, function() {
+  //     user.save(function(err, doc) {
+  //       if (err) {
+  //         return res.status(403).json({ success: false, error: err.message });
+  //       }
+  //
+  //       res.status(200).json(doc);
+  //     });
+  //   });
+  // });
 
-    if (!data) {
-      return res.status(403).json({ success: false, error: 'No data' });
-    }
-
-    async.each(Object.keys(data), function(key, callback) {
-      user[key] = data[key];
-      callback();
-    }, function() {
-      user.save(function(err, doc) {
-        if (err) {
-          return res.status(403).json({ success: false, error: err.message });
-        }
-
-        res.status(200).json(doc);
-      });
-    });
-  });
-
-  router.delete('/:user', app.middleware.ensureLogin, function(req, res) {
-    req.models.user.remove(function(err) {
-      if (err) {
-        return res.status(403).json({ success: false });
-      }
-
-      res.status(200).json({ success: true });
-    });
-  });
+  // router.delete('/:user', app.middleware.ensureLogin, function(req, res) {
+  //   req.models.user.remove(function(err) {
+  //     if (err) {
+  //       return res.status(403).json({ success: false });
+  //     }
+  //
+  //     res.status(200).json({ success: true });
+  //   });
+  // });
 
   return router;
 };
